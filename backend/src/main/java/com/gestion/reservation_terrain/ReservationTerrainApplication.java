5f415afaae4682b16d9a944c438ffddea7335e7f
mokaddemhicham <mokaddem.ensa@uhp.ac.ma>
2023-12-13 11:55:08 +0100

    create project structure

---
+++ backend/src/main/java/com/gestion/reservation_terrain/ReservationTerrainApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.reservation_terrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationTerrainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservationTerrainApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gestion/reservation_terrain/ReservationTerrainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.reservation_terrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationTerrainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
